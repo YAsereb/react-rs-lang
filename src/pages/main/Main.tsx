2310137ce13e208f1d960e992a6373ca230d4d28
YAsereb <cjiaba123@gmail.com>
2022-11-23 17:36:33 +0300

    remove description about our team

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -42,26 +42,6 @@ const MainPage = () => {
             />
           </div>
         </article>
-        <article className="article">
-          <h2 className="article-header">Наша команда</h2>
-          <div className="wrapper">
-            <div className="developer-card">
-              <h3 className="developer-name">Yaroslav Serebrenikov</h3>
-              <img src="./assets/img/proger1.png" className="developer-img" alt="developer_YAsereb" />
-              <p className="developer-description">Team Lead frontend developer <br />Разработал Аудио игру,список слов,учебник.Координировал команду.</p>
-            </div>
-            <div className="developer-card">
-              <h3 className="developer-name">Usevalad Kunski</h3>
-              <img src="./assets/img/proger2.png" className="developer-img" alt="developer_SEvk4a" />
-              <p className="developer-description" >Team Full stack developer <br />Развернул сервер,разработал авторизацию пользователей и роутинг.Разработал игру спринт.Участвовал в разработке статистики и дизайна.</p>
-            </div>
-            <div className="developer-card">
-              <h3 className="developer-name">Yauhen Makarevich</h3>
-              <img src="./assets/img/proger3.png" className="developer-img" alt="developer_zhentosmak" />
-              <p className="developer-description">Frontend developer <br /> Сделал дизайн и верстку главной страницы. Участвовал в разработке игры Спринт. </p>
-            </div>
-          </div>
-        </article>
       </main>
       <Footer />
     </>
